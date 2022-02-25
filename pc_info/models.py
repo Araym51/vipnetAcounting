e7717c0fa4f64929a554711257a3851ee1fc5f88
Egor Ostroumov <araimo@yandex.ru>
2022-02-25 12:10:07 +0300

    внесены изменения в модели

--- pc_info/models.py
+++ pc_info/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
 # Create your models here.
-import vipnet_users.models
-from vipnet_users.models import VipNetUsers
 
 
-class PCinfo(models.Model):
+class PcInfo(models.Model):
     WINDOWS_7 = 'Windows 7'
     WINDOWS_10 = 'Windows 10'
     ASTRA_LINUX = 'ASTRA_LINUX'
@@ -17,11 +15,12 @@ class PCinfo(models.Model):
     )
 
     pc_name = models.CharField(verbose_name='имя компьютера', max_length=64, unique=True)
-    operation_system = models.CharField(choices=OS_CHOICES, verbose_name='операционная система', max_length=24, default=WINDOWS_7)
+    operation_system = models.CharField(choices=OS_CHOICES, verbose_name='Операционная система', max_length=24, default=WINDOWS_7)
     hdd_number = models.CharField(verbose_name='Учетный номер ЖМД', max_length=32, blank=False)
     hdd_serial = models.CharField(verbose_name='Серийный номер ЖМД', max_length=64, blank=False)
-    inventory_number = models.CharField(verbose_name='инвентарный номер', max_length=64, blank=False, unique=True)
-    pc_model = models.CharField(verbose_name='модель ПЭВМ', max_length=64, blank=True)
+    inventory_number = models.CharField(verbose_name='Инвентарный номер', max_length=64, blank=False, unique=True)
+    pc_model = models.CharField(verbose_name='Модель ПЭВМ', max_length=64, blank=True)
     pc_mac = models.CharField(verbose_name='MAC адрес ПЭВМ', max_length=32)
-    kaspersky = models.BooleanField(verbose_name='начличие антивируса Касперский', default=True)
-    username = models.ForeignKey(vipnet_users.models.VipNetUsers, on_delete=models.CASCADE)
+    kaspersky = models.BooleanField(verbose_name='Начличие антивируса Касперский', default=True)
+    vipnet_name = models.CharField(verbose_name='Название VipNet ключа', max_length=64)
+    is_active = models.BooleanField(verbose_name='Активен', default=True)
--- vipnet_users/models.py
+++ vipnet_users/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
-
-import location.models
 from location.models import Town, Street
+from pc_info.models import PcInfo
 
 
-class VipNetUsers(AbstractUser):
+class VipNetUsers(models.Model):
     username = models.EmailField(unique=True)
     first_name = models.CharField(verbose_name='Имя', max_length=150)
     last_name = models.CharField(verbose_name='Фамилия', max_length=150)
     fathers_name = models.CharField(verbose_name='Отчество', max_length=150)
-    division = models.CharField(verbose_name='подразделение', max_length=150)
-    position = models.CharField(verbose_name='должность', max_length=150)
-    town_name = models.ForeignKey(location.models.Town, on_delete=models.CASCADE)
-    street = models.ForeignKey(location.models.Street, on_delete=models.CASCADE)
-    office_number = models.ForeignKey(location.models.Street, on_delete=models.CASCADE)
+    division = models.CharField(verbose_name='Подразделение', max_length=150)
+    position = models.CharField(verbose_name='Должность', max_length=150)
+    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+    vipnet_name = models.ForeignKey(PcInfo, on_delete=models.CASCADE)
